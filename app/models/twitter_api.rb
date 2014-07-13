969cc6f0ab4340723c7588f5c389c8c30d171cf8
Eric Iacutone <eric.iacutone@gmail.com>
2014-07-13 00:32:25 -0400

    user a method instead of initialize for twitter_api class

--- app/models/twitter_api.rb
+++ app/models/twitter_api.rb
@@ -1,19 +1,17 @@
 class TwitterApi
-	
-attr_reader :initiate_client
 
-def initialize
-	@initiate_client = 
+	def initiate_client
 		Twitter::REST::Client.new do |config|
 	    config.consumer_key = ENV['TWITTER_KEY']
 	    config.consumer_secret = ENV['TWITTER_SECRET']
 	    config.access_token  = ENV['TWITTER_OATH_TOKEN']
 	    config.access_token_secret = ENV['TWITTER_OATH_TOKEN_SECRET']
 	  end
-end
+	end
 	
 	def find_user(name)
 		client = initiate_client
 		client.user(name)
 	end
+	
 end
