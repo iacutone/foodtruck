7863600db312ae2953411a44ed92c83d990a5fd3
Eric Iacutone <eric.iacutone@gmail.com>
2014-07-22 22:35:12 -0400

    make after tweet save instantiate parser object

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -13,8 +13,15 @@ class Tweet
   
   belongs_to :truck
   
+  after_save :determine_location
+  
   def as_indexed_json(options={})
     as_json(except: [:id, :_id])
+  end
+  
+  def determine_location
+    update = Parser.new
+    update.abstract_location(self.twitter_text)
   end
   
 end
